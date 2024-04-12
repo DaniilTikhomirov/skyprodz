4a04c6323161d1cc74cb7cb301f03d4f43f51c49
Daniil Tikhomirov <daniltihomirov946@gmail.com>
2024-04-12 13:57:11 +0300

    start

--- src/widget.py
+++ src/widget.py
@@ -2,17 +2,25 @@ from src.mask import security_card, security_num
 
 
 def mask_card_and_num(name_and_number: str) -> str:
-    num = []
-    alpha = []
+    """проверяет номер это или счет и маскирует карту или счет"""
+    num_list = []
+    alpha_list = []
     for word in name_and_number:
         if word.isalpha():
-            alpha.append(word)
+            alpha_list.append(word)
         elif word.isdigit():
-            num.append(word)
-    num = "".join(num)
-    alpha = "".join(alpha)
+            num_list.append(word)
+    num = "".join(num_list)
+    alpha = "".join(alpha_list)
     if alpha.lower() == "счет" or alpha.lower() == "счёт":
         return security_num(num)
     else:
         return security_card(num)
 
+
+def format_date(date_time: str) -> str:
+    """форматирует дату и время"""
+    date_time_list = date_time.split("-")
+    date_time_list[-1] = date_time_list[-1].split("T")[0]
+    date_time = ".".join(date_time_list)
+    return date_time
--- test/test_func.py
+++ test/test_func.py
@@ -1,19 +1,23 @@
 from src.mask import security_card, security_num
-from src.widget import mask_card_and_num
+from src.widget import format_date, mask_card_and_num
 
-test = ["Maestro 1596837868705199",
-        "Счет 64686473678894779589",
-        "MasterCard 7158300734726758",
-        "Счет 35383033474447895560",
-        "Visa Classic 6831982476737658",
-        "Visa Platinum 8990922113665229",
-        "Visa Gold 5999414228426353",
-        "Счет 73654108430135874305"]
+test = [
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+]
 
-# print(security_card("1234123412341234"))
-# print(security_num("73654108430135874305"))
-# print(security_card("1234123412341234857"))
-# print(security_num("73654108430135874305579"))
+print(security_card("1234123412341234"))
+print(security_num("73654108430135874305"))
+print(security_card("1234123412341234857"))
+print(security_num("73654108430135874305579"))
 
 for line in test:
     print(mask_card_and_num(line))
+
+print(format_date("2018-07-11T02:26:18.671407"))
