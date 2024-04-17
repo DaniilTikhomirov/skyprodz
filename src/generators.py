9bd727c84e392c35059b5eb799a205d90dfc93ed
Daniil Tihomirov <daniltihomirov946@gmail.com>
2024-04-17 22:40:00 +0300

    add function

---
+++ src/generators.py
@@ -0,0 +1,17 @@
+def filter_by_currency(transactions: list, value: str) -> object:
+    """фильтрует по валюте и воврощает обьект"""
+    return filter(lambda x: x["operationAmount"]["currency"]["code"] == value.upper(), transactions)
+
+
+def transaction_descriptions(transactions: list) -> object:
+    """возврощает обьект с операциями"""
+    return (item["description"] for item in transactions)
+
+
+def card_number_generator(start, end):
+    """генирирует номера карт"""
+    return (
+        str(10000_0000_0000_0000 + i)[1:5] +
+        " " + str(10000_0000_0000_0000 + i)[5:9] +
+        " " + str(10000_0000_0000_0000 + i)[9:13] +
+        " " + str(10000_0000_0000_0000 + i)[13:] for i in range(start, end + 1))
