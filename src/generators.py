855ba819d2d1c8601e86950ee2f7fd31355158c2
Daniil Tihomirov <daniltihomirov946@gmail.com>
2024-04-17 23:12:33 +0300

    small fix

--- src/generators.py
+++ src/generators.py
@@ -1,17 +1,25 @@
-def filter_by_currency(transactions: list, value: str) -> object:
+import typing
+
+
+def filter_by_currency(transactions: list, value: str) -> typing.Iterator:
     """фильтрует по валюте и воврощает обьект"""
     return filter(lambda x: x["operationAmount"]["currency"]["code"] == value.upper(), transactions)
 
 
-def transaction_descriptions(transactions: list) -> object:
+def transaction_descriptions(transactions: list) -> typing.Generator:
     """возврощает обьект с операциями"""
     return (item["description"] for item in transactions)
 
 
-def card_number_generator(start, end):
+def card_number_generator(start: int, end: int) -> typing.Generator:
     """генирирует номера карт"""
     return (
-        str(10000_0000_0000_0000 + i)[1:5] +
-        " " + str(10000_0000_0000_0000 + i)[5:9] +
-        " " + str(10000_0000_0000_0000 + i)[9:13] +
-        " " + str(10000_0000_0000_0000 + i)[13:] for i in range(start, end + 1))
+        str(10000_0000_0000_0000 + i)[1:5]
+        + " "
+        + str(10000_0000_0000_0000 + i)[5:9]
+        + " "
+        + str(10000_0000_0000_0000 + i)[9:13]
+        + " "
+        + str(10000_0000_0000_0000 + i)[13:]
+        for i in range(start, end + 1)
+    )
