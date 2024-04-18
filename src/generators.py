2394cbae583033fa03eaaa30195035194c99f47c
Daniil Tihomirov <daniltihomirov946@gmail.com>
2024-04-18 23:41:24 +0300

    small fix

--- src/generators.py
+++ src/generators.py
@@ -3,7 +3,8 @@ import typing
 
 def filter_by_currency(transactions: list, value: str) -> typing.Iterator:
     """фильтрует по валюте и воврощает обьект"""
-    return filter(lambda x: x["operationAmount"]["currency"]["code"] == value.upper(), transactions)
+    for item in filter(lambda x: x["operationAmount"]["currency"]["code"] == value.upper(), transactions):
+        yield item
 
 
 def transaction_descriptions(transactions: list) -> typing.Generator:
@@ -13,13 +14,13 @@ def transaction_descriptions(transactions: list) -> typing.Generator:
 
 def card_number_generator(start: int, end: int) -> typing.Generator:
     """генирирует номера карт"""
-    return (
-        str(10000_0000_0000_0000 + i)[1:5]
-        + " "
-        + str(10000_0000_0000_0000 + i)[5:9]
-        + " "
-        + str(10000_0000_0000_0000 + i)[9:13]
-        + " "
-        + str(10000_0000_0000_0000 + i)[13:]
-        for i in range(start, end + 1)
-    )
+    for i in range(start, end + 1):
+        yield (
+            str(10000_0000_0000_0000 + i)[1:5]
+            + " "
+            + str(10000_0000_0000_0000 + i)[5:9]
+            + " "
+            + str(10000_0000_0000_0000 + i)[9:13]
+            + " "
+            + str(10000_0000_0000_0000 + i)[13:]
+        )
