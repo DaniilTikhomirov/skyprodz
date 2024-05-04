3ff7fd924647722a92e59fff68ca0fee651e3653
Daniil Tikhomirov <daniltihomirov946@gmail.com>
2024-05-04 17:13:40 +0300

    small fix

--- src/currency.py
+++ src/currency.py
@@ -4,14 +4,51 @@ from datetime import datetime
 from src.utils import write_xml_from_web
 
 # словарь код валюты: ID валюты
-CODE_CURRENCY = {'AUD': 'R01010', 'AZN': 'R01020A', 'GBP': 'R01035', 'AMD': 'R01060', 'BYN': 'R01090B', 'BGN': 'R01100',
-                 'BRL': 'R01115', 'HUF': 'R01135', 'VND': 'R01150', 'HKD': 'R01200', 'GEL': 'R01210', 'DKK': 'R01215',
-                 'AED': 'R01230', 'USD': 'R01235', 'EUR': 'R01239', 'EGP': 'R01240', 'INR': 'R01270', 'IDR': 'R01280',
-                 'KZT': 'R01335', 'CAD': 'R01350', 'QAR': 'R01355', 'KGS': 'R01370', 'CNY': 'R01375', 'MDL': 'R01500',
-                 'NZD': 'R01530', 'NOK': 'R01535', 'PLN': 'R01565', 'RON': 'R01585F', 'XDR': 'R01589', 'SGD': 'R01625',
-                 'TJS': 'R01670', 'THB': 'R01675', 'TRY': 'R01700J', 'TMT': 'R01710A', 'UZS': 'R01717', 'UAH': 'R01720',
-                 'CZK': 'R01760', 'SEK': 'R01770', 'CHF': 'R01775', 'RSD': 'R01805F', 'ZAR': 'R01810', 'KRW': 'R01815',
-                 'JPY': 'R01820'}
+CODE_CURRENCY = {
+    "AUD": "R01010",
+    "AZN": "R01020A",
+    "GBP": "R01035",
+    "AMD": "R01060",
+    "BYN": "R01090B",
+    "BGN": "R01100",
+    "BRL": "R01115",
+    "HUF": "R01135",
+    "VND": "R01150",
+    "HKD": "R01200",
+    "GEL": "R01210",
+    "DKK": "R01215",
+    "AED": "R01230",
+    "USD": "R01235",
+    "EUR": "R01239",
+    "EGP": "R01240",
+    "INR": "R01270",
+    "IDR": "R01280",
+    "KZT": "R01335",
+    "CAD": "R01350",
+    "QAR": "R01355",
+    "KGS": "R01370",
+    "CNY": "R01375",
+    "MDL": "R01500",
+    "NZD": "R01530",
+    "NOK": "R01535",
+    "PLN": "R01565",
+    "RON": "R01585F",
+    "XDR": "R01589",
+    "SGD": "R01625",
+    "TJS": "R01670",
+    "THB": "R01675",
+    "TRY": "R01700J",
+    "TMT": "R01710A",
+    "UZS": "R01717",
+    "UAH": "R01720",
+    "CZK": "R01760",
+    "SEK": "R01770",
+    "CHF": "R01775",
+    "RSD": "R01805F",
+    "ZAR": "R01810",
+    "KRW": "R01815",
+    "JPY": "R01820",
+}
 
 
 def get_currencies(currency: str) -> Decimal:
@@ -21,7 +58,7 @@ def get_currencies(currency: str) -> Decimal:
     :return: цена валюты
     """
     if currency == "RUB":
-        return Decimal('1')
+        return Decimal("1")
     # получает актуальную дату
     time_now = datetime.strftime(datetime.now(), "%d/%m/%Y")
     # использую дату как ссылку для xml данных центра банка
@@ -34,9 +71,10 @@ def get_currencies(currency: str) -> Decimal:
     root = three.getroot()
     # проходимся по корню
     for child in root:
-        if child.attrib['ID'] == CODE_CURRENCY[currency]:
-            return Decimal(child.find('Value').text.replace(',', '.'))
-    return Decimal('0')
+        if child.attrib["ID"] == CODE_CURRENCY[currency]:
+            print(type(child.find("Value")))
+            return Decimal(str(child.find("Value").text).replace(",", "."))
+    return Decimal("0")
 
 
 def calculate_amount_in_rub(operation: dict) -> Decimal:
@@ -45,7 +83,7 @@ def calculate_amount_in_rub(operation: dict) -> Decimal:
     :param operation: операция
     :return: сумма операции в рублях
     """
-    code_currency = operation['operationAmount']['currency']['code']
-    amount = operation['operationAmount']['amount']
+    code_currency = operation["operationAmount"]["currency"]["code"]
+    amount = operation["operationAmount"]["amount"]
     amount_in_rub = Decimal(str(amount)) * get_currencies(code_currency)
     return amount_in_rub
--- test/test_utils.py
+++ test/test_utils.py
@@ -3,7 +3,6 @@ from src.utils import unpack_json
 from unittest.mock import Mock
 
 
-
 @pytest.fixture
 def data() -> list[dict]:
     return [
@@ -11,32 +10,20 @@ def data() -> list[dict]:
             "id": 441945886,
             "state": "EXECUTED",
             "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {
-                "amount": "31957.58",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
             "description": "Перевод организации",
             "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589"
+            "to": "Счет 64686473678894779589",
         },
         {
             "id": 41428829,
             "state": "EXECUTED",
             "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {
-                "amount": "8221.37",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560"
-        }
+            "to": "Счет 35383033474447895560",
+        },
     ]
 
 
