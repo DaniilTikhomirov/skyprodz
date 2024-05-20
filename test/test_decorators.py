250d5cbdba5825854c54b5a053e1e7277b7c0b80
Daniil Tikhomirov <daniltihomirov946@gmail.com>
2024-05-20 17:59:01 +0300

    fix test

--- test/test_decorators.py
+++ test/test_decorators.py
@@ -1,11 +1,10 @@
 import datetime
 from typing import Any
+from unittest.mock import patch
 
 import pytest
 
 from src.decorators import log
-
-time = str(datetime.datetime.now().strftime("%m-%d-%y %H:%M:%S"))
 
 
 @log(filename="t2.txt")
@@ -22,19 +21,31 @@ def sum_2(a: Any, b: Any) -> Any:
 
 @pytest.mark.parametrize(
     "a, b, answer",
-    [(1, 2, f"{time} my_function ok"), (1, "2", f"{time} my_function error: TypeError Input:(1, '2'), {'{}'}")],
+    [
+        (1, 2, "05-20-24 17:43:56 my_function ok"),
+        (1, "2", "05-20-24 17:43:56 my_function error: TypeError Input:(1, '2'), {}"),
+    ],
 )
 def test_log_file(a: str | int, b: str | int, answer: str) -> None:
-    sum_(a, b)
-    with open("t2.txt", "r", encoding="utf-8") as f:
-        for line in f:
-            pass
-    assert line.strip() == answer
+    target = datetime.datetime(2024, 5, 20, 17, 43, 56)
+    with patch("datetime.datetime") as mock_now:
+        mock_now.now.return_value = target
+        sum_(a, b)
+        with open("t2.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                pass
+        assert line.strip() == answer
 
 
 @pytest.mark.parametrize(
     "a, b, answer",
-    [(1, 2, f"{time} my_function ok"), (1, "2", f"{time} my_function error: TypeError Input:(1, '2'), {'{}'}")],
+    [
+        (1, 2, "05-20-24 17:43:56 my_function ok"),
+        (1, "2", "05-20-24 17:43:56 my_function error: TypeError Input:(1, '2'), {}"),
+    ],
 )
 def test_log_console(a: str | int, b: str | int, answer: str) -> None:
-    assert sum_2(a, b) == answer
+    target = datetime.datetime(2024, 5, 20, 17, 43, 56)
+    with patch("datetime.datetime") as mock_now:
+        mock_now.now.return_value = target
+        assert sum_2(a, b) == answer
