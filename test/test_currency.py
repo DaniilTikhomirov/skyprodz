cb85065765e8bf64b21b40054c272eab3f625d1e
Daniil Tikhomirov <daniltihomirov946@gmail.com>
2024-05-20 17:59:26 +0300

    add test

--- test/test_currency.py
+++ test/test_currency.py
@@ -1,15 +1,15 @@
-import os.path
-import pytest
-
 import xml.etree.ElementTree as ET
-from src.currency import get_currencies, calculate_amount_in_rub
 from decimal import Decimal
 from unittest.mock import patch
 
+import pytest
+
+from src.currency import calculate_amount_in_rub, get_currencies
+
 
 @pytest.fixture
-def data():
-    return '''<?xml version="1.0" encoding="windows-1251"?>
+def data() -> str:
+    return """<?xml version="1.0" encoding="windows-1251"?>
 <ValCurs Date="15.05.2024" name="Foreign Currency Market">
     <Valute ID="R01235">
         <NumCode>840</NumCode>
@@ -25,32 +25,52 @@ def data():
         <Name>Евро</Name>
         <Value>90,1234</Value>
     </Valute>
-</ValCurs>'''
+</ValCurs>"""
 
 
 def test_get_currencies_rub() -> None:
     assert get_currencies("RUB") == Decimal("1")
 
 
-def test_get_currencies(data) -> None:
-    with patch('builtins.open') as mock_open:
-        with patch('xml.etree.ElementTree.parse') as parse_mock:
-            mock_tree = ET.fromstring(data)
+def test_get_currencies(data: str) -> None:
+    with patch("builtins.open") as mock_open:
+        with patch("xml.etree.ElementTree.parse") as parse_mock:
+            mock_tree = ET.ElementTree(ET.fromstring(data))
             parse_mock.return_value = mock_tree
             mock_file = mock_open.return_value.__enter__.return_value
             mock_file.read.return_value = data
-            value = get_currencies('USD')
-            assert value == Decimal("74,3250")
+            value = get_currencies("USD")
+            assert value == Decimal("74.3250")
 
 
-# def test_calculate_amount_in_rub() -> None:
-#     dict_operation = {
-#         "id": 649467725,
-#         "state": "EXECUTED",
-#         "date": "2018-04-14T19:35:28.978265",
-#         "operationAmount": {"amount": "96995.73", "currency": {"name": "руб.", "code": "RUB"}},
-#         "description": "Перевод организации",
-#         "from": "Счет 27248529432547658655",
-#         "to": "Счет 97584898735659638967",
-#     }
-#     assert calculate_amount_in_rub(dict_operation) == Decimal("96995.73")
+def test_calculate_amount_in_rub(data: str) -> None:
+    with patch("builtins.open") as mock_open:
+        with patch("xml.etree.ElementTree.parse") as parse_mock:
+            mock_tree = ET.ElementTree(ET.fromstring(data))
+            parse_mock.return_value = mock_tree
+            mock_file = mock_open.return_value.__enter__.return_value
+            mock_file.read.return_value = data
+            operation = {
+                "id": 782295999,
+                "state": "EXECUTED",
+                "date": "2019-09-11T17:30:34.445824",
+                "operationAmount": {"amount": "54280.01", "currency": {"name": "USD", "code": "USD"}},
+                "description": "Перевод организации",
+                "from": "Счет 24763316288121894080",
+                "to": "Счет 96291777776753236930",
+            }
+            value = calculate_amount_in_rub(operation)
+            assert value == Decimal("4034361.743250")
+
+
+def test_calculate_amount_in_rub_RUB() -> None:
+    operation = {
+        "id": 782295999,
+        "state": "EXECUTED",
+        "date": "2019-09-11T17:30:34.445824",
+        "operationAmount": {"amount": "54280.01", "currency": {"name": "RUB", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Счет 24763316288121894080",
+        "to": "Счет 96291777776753236930",
+    }
+    assert calculate_amount_in_rub(operation) == Decimal("54280.01")
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,17 +1,17 @@
 import json
+import os.path
 from datetime import datetime
+from unittest.mock import Mock, patch
 
 from src.utils import unpack_json, write_xml_from_web
-from unittest.mock import Mock, patch
-import os.path
 
 
-@patch('builtins.open')
+@patch("builtins.open")
 def test_unpack_json(mock_open: Mock) -> None:
     mock_file = mock_open.return_value.__enter__.return_value
     mock_file.read.return_value = json.dumps([{"test": "test"}])
     assert unpack_json(os.path.join("..", "data", "operations.json"))
-    mock_open.assert_called_once_with(os.path.join("..", "data", "operations.json"), 'r', encoding="utf8")
+    mock_open.assert_called_once_with(os.path.join("..", "data", "operations.json"), "r", encoding="utf8")
 
 
 def test_write_xml_from_web() -> None:
@@ -20,6 +20,5 @@ def test_write_xml_from_web() -> None:
             time_now = datetime.strftime(datetime.now(), "%d/%m/%Y")
             url = f"https://cbr.ru/scripts/XML_daily.asp?date_req={time_now}"
             write_xml_from_web(url, "cbr")
-            mock_open.assert_called_once_with(os.path.join("..", "data", "cbr.xml"), 'wb')
-            mock_get.assert_called_once_with(f'https://cbr.ru/scripts/XML_daily.asp?date_req={time_now}')
-
+            mock_open.assert_called_once_with(os.path.join("..", "data", "cbr.xml"), "wb")
+            mock_get.assert_called_once_with(f"https://cbr.ru/scripts/XML_daily.asp?date_req={time_now}")
